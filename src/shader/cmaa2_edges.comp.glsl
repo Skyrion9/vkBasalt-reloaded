b5465ad98ee0143bcfe721a70ff3302deb4065d3
Skyrion9 <ottomanobi@gmail.com>
2026-09-23 02:25:47 +0300

    docs(license): add CMAA2 license headers & Apache-2.0 license information

--- src/shader/cmaa2_edges.comp.glsl
+++ src/shader/cmaa2_edges.comp.glsl
@@ -1,3 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018, Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Original Author(s): Filip Strugar, Adam Lake (Intel Corporation)
+// Original Source:    https://github.com/GameTechDev/CMAA2
+//
+// MODIFICATIONS BY Skyrion9/vkBasalt-reloaded:
+// - Ported from DirectX 12 HLSL to Vulkan GLSL compute shaders.
+// - Replaced per thread global atomics with GL_KHR_shader_subgroup_ballot reduction in canditates to reduce contention.
+// - Added inplace slice optimization for the vkBasalt effect chain architecture.
+// - Replaced compile time HLSL macros with Vulkan specialization constants with more configuration knobs.
+// - Integrated with vkBasalt-reloaded's color_space.h for HDR (PQ/HLG) and SDR edge detection.
+// - Removed MSAA support (vkBasalt-reloaded operates on the final resolved swapchain image).
+// - Added cache routing qualifiers and conditional helper compilation for glslang.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #version 450
 #extension GL_GOOGLE_include_directive : enable
 #extension GL_KHR_shader_subgroup_basic : enable
